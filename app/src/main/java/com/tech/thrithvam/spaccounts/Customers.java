591be243c280e7d003d3e0049acb767469e05a91
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-07-26 12:20:07 +0530

    customers screen

---
+++ app/src/main/java/com/tech/thrithvam/spaccounts/Customers.java
@@ -0,0 +1,13 @@
+package com.tech.thrithvam.spaccounts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Customers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customers);
+    }
+}
