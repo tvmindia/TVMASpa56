da0d47a55457983d49be863543e067784102b459
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-08-03 15:56:28 +0530

    supplier details

--- app/src/main/java/com/tech/thrithvam/spaccounts/SupplierDetails.java
+++ app/src/main/java/com/tech/thrithvam/spaccounts/SupplierDetails.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -133,5 +135,24 @@ public class SupplierDetails extends AppCompatActivity {
             asyncTasks.get(i).cancel(true);
         }
         super.onBackPressed();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_home) {
+            Intent intent=new Intent(this,HomeScreen.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
